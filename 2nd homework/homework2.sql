36df53ac705e1e26069c4a509a5d01b5988d4159
isokovq <qahramonisokov@gmail.com>
2025-01-17 22:55:20 +0500

    Add files via upload

---
+++ 2nd homework/homework2.sql
@@ -0,0 +1,14 @@
+create database lesson2
+go 
+use lessoon2
+EXEC sp_msforeachdb 
+
+EXEC sp_MSforeachdb @command1 = '
+USE [?];
+SELECT 
+    ''?'' AS DatabaseName,
+    TABLE_SCHEMA AS SchemaName,
+    TABLE_NAME AS TableName
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = ''BASE TABLE'';
+';
